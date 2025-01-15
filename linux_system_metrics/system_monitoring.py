61630d091be79c6ee7a53f72a7334d8a2f425a44
AnnLinnden <annlinnden@gmail.com>
2025-01-15 16:36:38 +0600

    Добавила отслеживание системных метрик

--- linux_system_metrics/system_monitoring.py
+++ linux_system_metrics/system_monitoring.py
@@ -1,5 +1,15 @@
 import psutil
 
 
-class SystemMetricsMonitor():
-    pass
+class SystemMetricsMonitor:
+    def get_system_metrics(self):
+        cpu = psutil.cpu_percent(interval=0)
+        ram = psutil.virtual_memory()
+        rom = psutil.disk_usage('/')
+        return {
+            "cpu": cpu,
+            "ram_used": round(ram.used / (1024 ** 3), 2),  # В ГБ
+            "ram_total": round(ram.total / (1024 ** 3), 2),  # В ГБ
+            "rom_used": round(rom.used / (1024 ** 3), 2),  # В ГБ
+            "rom_total": round(rom.total / (1024 ** 3), 2),  # В ГБ
+        }
